d000b468482f37bd42d9a870dae979fac18f3b38
Nick Weiland <njweiland23@gmail.com>
2023-11-20 18:53:53 -0600

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import About from "./About.js"
+import Home from "./Home.js"
+import NavBar from "./NavBar.js"
+
 
 function App() {
   return (
@@ -9,5 +13,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
